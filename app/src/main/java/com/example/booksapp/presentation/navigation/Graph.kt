f4fdf493c10c957479313cca32145b0053fa2d27
dnlxorr <79987425+dnlxorr@users.noreply.github.com>
2024-04-14 20:47:18 -0500

    Removing unused classes and variables. Improving LoginScreen behaviour. (#13) (#14)
    Co-authored-by: dnlxorr <dnlxorr@gmail.com>

--- app/src/main/java/com/example/booksapp/presentation/navigation/Graph.kt
+++
@@ -1,7 +0,0 @@
-package com.example.booksapp.presentation.navigation
-
-//object Graph {
-//    const val ROOT = "root_graph"
-//    const val AUTHENTICATION = "auth_graph"
-//    const val LANDING_PAGE = "home_graph"
-//}
